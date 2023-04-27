8cdc81751295c92d26b522fd526e5b8bb50e8443
Thomas Grant <thomas.grant42@hotmail.fr>
2023-04-27 03:25:37 -0700

    Adding some comments

--- Tictactoe_game.py
+++ Tictactoe_game.py
@@ -1,1 +1,33 @@
 
+'''
+-------------
+| 1 | 2 | 3 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| 7 | 8 | 9 |
+-------------
+'''
+
+# Rules of the game
+# 2 players, one plays with 'X', one plays with 'O'.
+# The players play one turn at a time, alternating 
+# each turn.
+# A win is defined by 3 'X' or 'O' in a row
+# The row can be horizontal, vertical, diagonal
+# If all the boxes are filled, and there is no win, 
+# it's a tie
+
+# Setup the game
+## Ask player's names
+## Set the 'X' and 'O'
+## Set the grid
+
+# Play until win or tie
+## Show the grid and tell who's turn it is
+## Ask where to place a mark
+## Check for end scenarios
+## repeat
+
+# Display the result
+## Say who won, or if there is a tie
